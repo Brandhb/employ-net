825aca4e761caeca54627dca9a9576789e352b33
user3 <user3@vdi.local>
2025-02-07 01:44:20 -0800

    y

--- next.config.mjs
+++ next.config.mjs
@@ -3,26 +3,15 @@ const nextConfig = {
   publicRuntimeConfig: {
     baseURL: process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000",
     googleAnalyticsId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-    
   },
-  webpack: config => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    return config
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
   },
   reactStrictMode: true,
   basePath: "",
   images: {
     unoptimized: true,
-  },
-  trailingSlash: true,
-  async redirects() {
-    return [
-      {
-        source: "/api/webhooks/:path*",
-        destination: "/api/webhooks/:path*",
-        permanent: false, // Ensures no redirection
-      },
-    ];
   },
 };
 
